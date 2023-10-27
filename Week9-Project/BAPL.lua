75182b28ed0e400cb30a390894d9dce6dd80963f
mhfriesen <143233320+mhfriesen@users.noreply.github.com>
2023-10-27 13:24:20 -0700

    Add files via upload

--- Week9-Project/BAPL.lua
+++ Week9-Project/BAPL.lua
@@ -3,20 +3,29 @@ local tests = require"tests"
 local n1, n2, options = 0, 0, {}
 io.write("BAPL (Basically Another Programming Language) Martin Friesen 2023-10-20\n")
 
-io.write("\nTests after functions were introduced.\n")
+io.write("\nBALP recurses!\n")
 options.supress_prnt_wrap = false
 options.trace_ast = false
 options.trace_code = false
 options.trace_interp = false
-n1, n2 = 2 , 99
+n1, n2 = 1 , 1
 BAPL.pl(take(tests.main_cases, n1, n2), options)
 
+---[[
 io.write("\nMandelbrot using for loops and supressing print wrap.\n")
 options.supress_prnt_wrap = true
 options.trace_ast = false
 options.trace_code = false
 options.trace_interp = false
-n1, n2 = 1 , 1
+n1, n2 = 2 , 2
+BAPL.pl(take(tests.main_cases, n1, n2), options)
+
+io.write("\nTests after functions were introduced.\n")
+options.supress_prnt_wrap = false
+options.trace_ast = false
+options.trace_code = false
+options.trace_interp = false
+n1, n2 = 3 , 99
 BAPL.pl(take(tests.main_cases, n1, n2), options)
 
 io.write("\nTests after statements were introduced.\n")
@@ -24,7 +33,7 @@ options.supress_prnt_wrap = false
 options.trace_ast = false
 options.trace_code = false
 options.trace_interp = false
-n1, n2 = 1 , 99
+n1, n2 = 3 , 99
 BAPL.pl(take(tests.prefunction_cases, n1, n2), options)
 
 io.write("\nTests when just expressions were available.\n")
@@ -33,4 +42,4 @@ options.trace_code = false
 options.trace_interp = false
 n1, n2 = 1 , 99
 BAPL.pl(take(tests.primordial_cases, n1, n2), options)
-
+--]]
